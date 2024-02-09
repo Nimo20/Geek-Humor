55f13b9be63c71774bc9b299cb553d78ecda20dd
dobeid <dialajo9@gmail.com>
2024-02-08 19:01:42 -0800

    added function to fetch geek joke with joke API

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // event listener with fx that is triggered when button is clicked
     generateBtn.addEventListener('click', function () {
-       // promise.all = fx that fetches data from the 2 APIs (at random)
+        // promise.all = fx that fetches data from the 2 APIs (at random)
         Promise.all([getUserJoke(), getUserEmoji()])
             .then(function (results) {
                 // results[0] is the jokeResult, and results[1] is the emojiResult
@@ -21,5 +21,24 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     });
 
-    
+
+    // fx to get a random geek joke
+    function getUserJoke() {
+        // geek joke API  
+        var apiUrl = 'https://geek-jokes.sameerkumar.website/api';
+        return fetch(apiUrl)
+            .then(function (response) {
+                // if result is valid, joke is displayed
+                if (response.ok) {
+                    return response.text();
+                // if result is not valid, error message is displayed 
+                } else {
+                    console.error('Error fetching a geek joke:', response.statusText);
+                    return 'Sorry, could not fetch a geek joke at the moment.';
+                }
+            });
+    }
+
+    // fx to get a random emoji
+
 });
